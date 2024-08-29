8cc0771484f71ceb315ba1c3bbe859f0c132fd08
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-08-30 03:22:02 +0530

    initial project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = async (req, res) => {
+  console.log("Signup router called");
+  res.send("Signup router called");
+};
+
+export const login = async (req, res) => {
+  console.log("Login router called");
+  res.send("Login router called");
+};
+
+export const logout = async (req, res) => {
+  console.log("Logout router called");
+  res.send("Logout router called");
+};
