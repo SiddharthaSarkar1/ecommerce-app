712242d67143bc69a80d48e768757f645dc31724
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-08-31 04:22:51 +0530

    added signup functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,60 @@
+import jsonwebtoken from "jsonwebtoken";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+		httpOnly: true, // prevent XSS attacks, cross site scripting attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		maxAge: 15 * 60 * 1000, // 15 minutes
+	});
+	res.cookie("refreshToken", refreshToken, {
+		httpOnly: true, // prevent XSS attacks, cross site scripting attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+	});
+}
+
 export const signup = async (req, res) => {
-  console.log("Signup router called");
-  res.send("Signup router called");
+  const { email, password, name } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    //Authenticate
+    const { accessToken, refreshToken } = generateTokens(user._id);
+
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import express from "express";
 
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -9,8 +11,13 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.json()); //this will allow us to parse the body of the request
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  
+  connectDB();
 });
