dbe17f4fa717c7c51b1236742d4124a1238d932e
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-09-03 02:56:34 +0530

    product controller with basic functionalities

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,24 @@
 import express from "express";
 
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+    createProduct,
+  deleteProduct,
+  getAllProducts,
+  getFeaturedProducts,
+  getProductsByCategory,
+  getRecomendedProducts,
+  toggleFeaturedProduct,
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Route();
+const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
+router.get("/recommendations", getRecomendedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import express from "express";
 
 import authRoutes from './routes/auth.route.js';
+import productRoutes from './routes/product.route.js';
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -14,7 +15,8 @@ const PORT = process.env.PORT;
 app.use(express.json()); //this will allow us to parse the body of the request
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
