2b02d256841e3481d8094f4e4bc9146046fd5a7a
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-09-04 03:28:03 +0530

    added cart and coupon functionalities

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import express from "express";
 
-import authRoutes from './routes/auth.route.js';
-import productRoutes from './routes/product.route.js';
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -17,9 +19,11 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupon", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  
+
   connectDB();
 });
